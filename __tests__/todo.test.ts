9ecc4b118df2e57eeb13a8927d326bc0fc053db7
4nth0ny1 <acatullo4@gmail.com>
2023-08-07 19:04:08 -0500

    added todo server tests but do not work

---
+++ __tests__/todo.test.ts
@@ -0,0 +1,44 @@
+import { type inferProcedureInput } from "@trpc/server";
+import type { inferRouterOutputs } from '@trpc/server'
+import { Todo, PrismaClient } from "@prisma/client";
+
+
+import { render, screen, waitFor } from "@testing-library/react";
+import { appRouter, type AppRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
+import { mockDeep } from "jest-mock-extended";
+
+
+
+describe("Todo Router", () => {
+    it("should render all the todos", async () => {
+        const prismaMock = mockDeep<PrismaClient>();
+
+        const mockOutput: Todo[] = [
+            {
+              id: "1",
+              content: "hello test todo",
+              done: false,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+              userId: "user-1"
+            },
+          ];
+
+        const ctx = await createTRPCContext({
+        session: {
+            user: { id: "123", userId: "123421423" },
+            expires: "1",
+        }, prisma: prismaMock,
+        });
+        const caller = appRouter.createCaller(ctx);
+
+        prismaMock.todo.findMany.mockResolvedValue(mockOutput);
+        
+   
+      const todos = await caller.todo.getAll()
+    
+      expect(todos).toHaveLength(1);
+    
+    });
+})
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,6 @@ import { z } from 'zod'
 import type {AppRouter} from './server/api/root'
 
 type RouterOutputs = inferRouterOutputs<AppRouter>;
-type allTodosOutput = RouterOutputs['todo']['getAll']
+export type allTodosOutput = RouterOutputs['todo']['getAll']
 
 export type Todo = allTodosOutput[number]
