2052552ab447251baaa27547baf833613294485a
4nth0ny1 <acatullo4@gmail.com>
2023-08-07 19:05:48 -0500

    fixed indentation

--- __tests__/todo.test.ts
+++ __tests__/todo.test.ts
@@ -11,34 +11,34 @@ import { mockDeep } from "jest-mock-extended";
 
 
 describe("Todo Router", () => {
-    it("should render all the todos", async () => {
-        const prismaMock = mockDeep<PrismaClient>();
+  it("should render all the todos", async () => {
+    const prismaMock = mockDeep<PrismaClient>();
 
-        const mockOutput: Todo[] = [
-            {
-              id: "1",
-              content: "hello test todo",
-              done: false,
-              createdAt: new Date(),
-              updatedAt: new Date(),
-              userId: "user-1"
-            },
-          ];
+    const mockOutput: Todo[] = [
+      {
+        id: "1",
+        content: "hello test todo",
+        done: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: "user-1"
+      },
+    ];
 
-        const ctx = await createTRPCContext({
-        session: {
-            user: { id: "123", userId: "123421423" },
-            expires: "1",
-        }, prisma: prismaMock,
-        });
-        const caller = appRouter.createCaller(ctx);
-
-        prismaMock.todo.findMany.mockResolvedValue(mockOutput);
-        
-   
-      const todos = await caller.todo.getAll()
+    const ctx = await createTRPCContext({
+      session: {
+        user: { id: "123", userId: "123421423" },
+        expires: "1",
+      }, prisma: prismaMock,
+      });
     
-      expect(todos).toHaveLength(1);
-    
-    });
+    const caller = appRouter.createCaller(ctx);
+
+    prismaMock.todo.findMany.mockResolvedValue(mockOutput);
+      
+    const todos = await caller.todo.getAll()
+  
+    expect(todos).toHaveLength(1);
+  
+  });
 })
